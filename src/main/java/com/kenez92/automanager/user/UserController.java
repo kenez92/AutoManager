8a7d6e46237561a666e73cd174b227a740dca2fb
Daniel <daniel.kaminski@smartive.app>
2021-12-09 22:02:42 +0100

    Correct html files and userController

--- src/main/java/com/kenez92/automanager/user/UserController.java
+++ src/main/java/com/kenez92/automanager/user/UserController.java
@@ -2,8 +2,8 @@ package com.kenez92.automanager.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,8 +20,8 @@ class UserController {
         return "users/user";
     }
 
-    @PostMapping("/createUser")
-    public String createUser(@RequestBody UserDto userDto) {
+    @PostMapping(value = "/createUser")
+    public String createUser(@ModelAttribute UserDto userDto) {
         UserDto createdUser = userService.createUser(userDto);
         return "redirect:createUser";
     }
